eb02bb1531c4fe05278792ef8a0c72e231748506
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-05-14 21:51:09 +0300

    createNewArticle

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const uuidv4 = require("uuidv4");
 app.use(express.json());
 const PORT = 5000;
 
@@ -34,7 +35,6 @@ app.get("/articles", (req, res) => {
 
 
 app.get("/articles/search_1", (req, res) => {
-    
     const author = req.query.author;
     const article = articles.filter((element) => element.author === author);
     res.status(200);
@@ -43,12 +43,17 @@ app.get("/articles/search_1", (req, res) => {
 
 app.get("/articles/:id", (req, res) => {
     const id = req.params.id
-    const article = articles.find(element => element.id === Number(id))
+    const article = articles.find((element) => element.id === Number(id))
     res.status(200);
     res.json(article);
 })
 
-
+app.post("/articles", async (req, res) => {
+    req.body.id = await uuidv4.uuid();
+    articles.push(req.body);
+    res.status(201);
+    res.json(req.body);
+});
 
 app.listen(PORT, () => {
     console.log(`The server is listening at port ${PORT}`);

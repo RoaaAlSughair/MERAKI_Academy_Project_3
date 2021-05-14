e926611ca67fa273de4a859a29eadf04df5192cb
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-05-14 16:21:43 +0300

    getAllArticles

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const PORT = 5000;
 
+const articles = [
+    {
+    id: 1,
+    title: 'How I learn coding?',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    {
+    id: 2,
+    title: 'Coding Best Practices',
+    description:
+    'Lorem, ipsum dolor sit, Quam, mollitia.',
+    author: 'Besslan',
+    },
+    {
+    id: 3,
+    title: 'Debugging',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    ];
 
-
-
+app.get("/articles", (req, res) => {
+    res.status(200);
+    res.json(articles);
+});
 
 app.listen(PORT, () => {
     console.log(`The server is listening at port ${PORT}`);
